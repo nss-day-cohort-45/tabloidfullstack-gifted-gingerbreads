a55b9caf8ae972e34ca9ed4cd0dde50c8a2a48f3
Meghan Semrad <mlsemrad753@gmail.com>
2021-04-15 14:24:24 -0500

    made and filled out model file for comments and made the repository

---
+++ Tabloid/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tabloid.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+        public UserProfileId UserProfileId { get; set; } 
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
