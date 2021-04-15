c05c3d5f28f56746c1a9e30321c6c4ce22df0248
Rickie Le <rickienthefishies@gmail.com>
2021-04-15 15:16:07 -0500

    API - gets tags

---
+++ Tabloid/Controllers/TagController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Repositories;
+using Tabloid.Models;
+
+namespace Tabloid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagRepository _tagRepo;
+        
+        public TagController(ITagRepository tagRepo)
+        {
+            _tagRepo = tagRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_tagRepo.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Tag tag)
+        {
+            _tagRepo.Add(tag);
+            return CreatedAtAction("Get", new { id = tag.Id }, tag);
+        }
+    }
+}
---
+++ Tabloid/Repositories/TagRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Tabloid.Models;
+using Tabloid.Utils;
+using Microsoft.Data.SqlClient;
+
+namespace Tabloid.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public List<Tag> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, Name 
+                        FROM Tag
+                        ORDER BY Name asc
+                    ";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var tags = new List<Tag>();
+                        while (reader.Read())
+                        {
+                            tags.Add(new Tag()
+                            {
+                                Id = DbUtils.GetInt(reader, "Id"),
+                                Name = DbUtils.GetString(reader, "Name")
+                            });
+                        }
+
+                        reader.Close();
+                        return tags;
+                    }
+                }
+            }
+        }
+
+        public void Add(Tag tag)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Tag (Id, Name)
+                    OUTPUT INSERTED.ID
+                    VALUES (@Id, @Name)
+                    ";
+
+                    DbUtils.AddParameter(cmd, "Id", tag.Id);
+                    DbUtils.AddParameter(cmd, "Name", tag.Name);
+
+                    tag.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+
+    }
+}
