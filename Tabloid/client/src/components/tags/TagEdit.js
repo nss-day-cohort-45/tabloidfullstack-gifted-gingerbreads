664586aed285b923db20900d6c8042e618b48b7f
Rickie Le <rickienthefishies@gmail.com>
2021-04-19 13:44:18 -0500

    merging main

--- Tabloid/client/src/components/tags/TagEdit.js
+++ Tabloid/client/src/components/tags/TagEdit.js
@@ -8,6 +8,19 @@ const TagEdit = () => {
     const { editTag, getTagById } = useContext(TagContext);
     const history = useHistory();
 
+    useEffect(() => {
+        if (tagId) {
+            getTagById(tagId)
+                .then((tag) => {
+                    setTag(tag)
+                    setIsLoading(false)
+                })
+        }
+        else {
+            setIsLoading(false);
+        }
+    }, []);
+
     // Edit, hold onto state of tag in this view
     const [tag, setTag] = useState({
         name: ""
@@ -19,28 +32,31 @@ const TagEdit = () => {
     // Returns an object
     const { tagId } = useParams();
 
-    const handleControlledInputChange = (e) => {
 
+    // When the input field changes, update state
+    // Causes a re-render and updates the DOM
+    const handleControlledInputChange = (e) => {
+        // When you change a state object or array,
+        // Always create a copy to make changes, and then set the state.
+
+        const newTag = { ...tag } // tag is an object with properties
+
+        // Set the property to the new value
+        newTag[e.target.name] = e.target.value
+
+        // Update the state with the new user's input
+        setTag(newTag)
     }
 
 
-
-
-
-    useEffect(() => {
-        getTagById(tagId)
-            .then((response) => {
-                setTag(response)
-            })
-    }, []);
-
-    console.log("log", tagId, tag)
-
     const handleEdit = () => {
-        editTag(tag.id)
-            .then(() => {
-                history.push("/tags")
+        if (tagId) {
+            editTag({
+                id: tag.id,
+                name: tag.name
             })
+                .then(() => history.push(`/tag/edit/${tag.id}`))
+        };
     };
 
     const BackToTagsPage = () => {
@@ -51,9 +67,9 @@ const TagEdit = () => {
         <div>
             <Form>
                 <h2>Edit the tag "{tag.name}"</h2>
-                <input placeholder={tag.name} ></input>
-                <Button onClick={BackToTagsPage}>Go Back</Button>
+                <input placeholder={tag.name} id="name"></input>
                 <Button onClick={handleEdit}>Save Changes</Button>
+                <Button onClick={BackToTagsPage}>Go Back</Button>
             </Form>
         </div>
     );
