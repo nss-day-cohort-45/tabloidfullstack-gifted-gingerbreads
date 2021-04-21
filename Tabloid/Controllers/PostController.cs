c3fa8a0c4380ba5178280d6c2e54b58fcc72ac70
Meghan Semrad <mlsemrad753@gmail.com>
2021-04-21 10:13:26 -0500

    fixed bug

--- Tabloid/Controllers/PostController.cs
+++ Tabloid/Controllers/PostController.cs
@@ -33,7 +33,7 @@ namespace Tabloid.Controllers
             return Ok(userPosts);
         }
 
-        [HttpGet("GetById/{postId}")]
+        [HttpGet("GetById")]
         public IActionResult GetById(int postId)
         {
             var post = _postRepository.GetPostById(postId);
