369e8d94e7a4148aea4d47da39bf7ace7bae914e
Barry Shovlin <bshovlin32@gmail.com>
2021-04-21 09:44:12 -0500

    created a getPostByCategoryId method in the api

---
+++ CategoryIdSQLQuery.sql
@@ -0,0 +1,7 @@
+﻿ SELECT p.Id AS PostId, p.Title, p.Content, p.ImageLocation, p.PublishDateTime, p.CategoryId,
+                        c.Id AS CategoryId, c.[Name],
+                        up.Id AS UserProfileId, up.DisplayName
+                        FROM Post p
+                        LEFT JOIN Category c ON c.Id = p.CategoryId
+                        LEFT JOIN UserProfile up ON up.Id = p.UserProfileId
+                        WHERE p.CategoryId = 1
--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -117,7 +117,58 @@ namespace Tabloid.Repositories
                 }
             }
         }
+        public List<Post> GetPostsByCategory(int CategoryId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT p.Id AS PostId, p.Title, p.Content, p.ImageLocation, p.PublishDateTime, p.CategoryId
+                        c.Id AS CategoryId, c.[Name],
+                        up.Id AS UserProfileId, up.DisplayName
+                        FROM Post p
+                        LEFT JOIN Category c ON c.Id = p.CategoryId
+                        LEFT JOIN UserProfile up ON up.Id = p.UserProfileId
+                        WHERE p.CategoryId = @id";
 
+
+                    cmd.Parameters.AddWithValue("@id", CategoryId);
+                    var reader = cmd.ExecuteReader();
+
+                    var posts = new List<Post>();
+
+                    while (reader.Read())
+                    {
+                        posts.Add(new Post()
+                        {
+                            Id = DbUtils.GetInt(reader, "PostId"),
+                            Title = DbUtils.GetString(reader, "Title"),
+                            Content = DbUtils.GetString(reader, "Content"),
+                            ImageLocation = DbUtils.GetString(reader, "ImageLocation"),
+                            PublishDateTime = DbUtils.GetDateTime(reader, "PublishDateTime").ToString("MM/dd/yyyy"),
+                            CategoryId = DbUtils.GetInt(reader, "CategoryId"),
+                            PostCategory = new Category()
+                            {
+                                Id = DbUtils.GetInt(reader, "CategoryId"),
+                                Name = DbUtils.GetString(reader, "Name")
+                            },
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            PostAuthor = new UserProfile()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserProfileId"),
+                                DisplayName = DbUtils.GetString(reader, "DisplayName")
+                            }
+
+                        });
+
+                    }
+                    reader.Close();
+                    return posts;
+                }
+            }
+        }
         public List<Post> GetPostById(int PostId)
         {
             using (var conn = Connection)
